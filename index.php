07f085deebcc6c063d26a89759ebb2fe0e6ae5d0
DimasAdlan13 <139360477+DimasAdlan13@users.noreply.github.com>
2024-01-08 14:35:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ header('Location: login.php');
 <html>
 
 <head>
-    <title>Aplikasi Data Pembeli</title>
+    <title>Aplikasi Data Pembeliannnn</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body {
